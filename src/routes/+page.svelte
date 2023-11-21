9e95776154edcbcf014da90bf2a4a86c04ca4a4d
naveench909 <naveench909@gmail.com>
2023-11-21 15:57:52 +0530

    added page title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
-    import Main from "../components/Main.svelte";
+  import Main from "../components/Main.svelte";
 </script>
+
+<svelte:head>
+  <title>Naveen Portfolio</title>
+  <meta name="description" content="naveen portfolio svelte sveltekit" />
+</svelte:head>
 
 <Main />
